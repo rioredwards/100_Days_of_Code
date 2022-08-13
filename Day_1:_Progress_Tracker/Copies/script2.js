6f2cafa9154ef55ad49786cf2eeb6f0cf10a3b0b
Rio Edwards <rioredwards@gmail.com>
2022-08-13 12:45:10 -0700

    Finish Youtube Homepage

---
+++ Day_1:_Progress_Tracker/Copies/script2.js
@@ -0,0 +1,39 @@
+"use strict";
+let elementID = 0;
+
+function createNewListElement() {
+    elementID++;
+    let newListElement = document.createElement("LI");
+    newListElement.classList.add("list_element");
+    newListElement.setAttribute(`id`, `element_${elementID}`);
+    newListElement.innerHTML = `
+    <input
+    id="checkbox_${elementID}"
+    class="css-checkbox"
+    type="checkbox"
+    name="entry_${elementID}"
+    />
+    <label
+    for="checkbox_${elementID}"
+    name="checkbox_${elementID}_lbl"
+    class="css-label lite-green-check"
+    ></label>
+    <div class="list_box">
+        <input class="user_entry" type="text"></input>
+    </div>
+    <div class="button_container">
+        <button id="new_element_btn" onclick="addListElement()"><i class="fa-solid fa-plus"></i></button>
+        <button id="delete_element_btn" onclick="deleteListElement(${elementID})"><i class="fa-solid fa-minus"></i></button>
+    </div>`;
+    return newListElement;
+}
+
+function addListElement() {
+    let newListElement = createNewListElement();
+    document.getElementById("list_section").appendChild(newListElement);
+}
+
+function deleteListElement(id) {
+    let removeListElement = document.getElementById(`element_${id}`);
+    removeListElement.remove();
+}
---
+++ Day_2_YouTube_HomePage/plan.md
@@ -0,0 +1,8 @@
+⋆｡ﾟ ☁︎｡⋆｡ ﾟ ☾ ﾟ｡⋆ Youtube HomePage ⋆｡ﾟ ☁︎｡⋆｡ ﾟ ☾ ﾟ｡⋆
+
+        ~Layout using Grid and Flexbox~
+          ~Dummy Thicc Text and Icons~
+                ~Colors Match~
+                    ~Yes~
+
+⋆｡ﾟ ☁︎｡⋆｡ ﾟ ☾ ﾟ｡⋆ Youtube HomePage ⋆｡ﾟ ☁︎｡⋆｡ ﾟ ☾ ﾟ｡⋆
