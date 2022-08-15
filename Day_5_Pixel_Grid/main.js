88628aaeee6de065aa65db95143abcc2638b7fcb
Rio Edwards <rioredwards@gmail.com>
2022-08-14 20:40:01 -0700

    Day 5 Finish Pixel Grid

--- Day_5_Pixel_Grid/main.js
+++ Day_5_Pixel_Grid/main.js
@@ -1,62 +1,53 @@
 "using strict";
 
 let grid = document.getElementById(`pixel-container`);
-
-// let pixelArray = [
-//   { name: `1`, color: `white` },
-//   { name: `2`, color: `black` },
-//   { name: `3`, color: `black` },
-//   { name: `4`, color: `black` },
-//   { name: `5`, color: `black` },
-//   { name: `6`, color: `white` },
-//   { name: `7`, color: `black` },
-//   { name: `8`, color: `black` },
-//   { name: `9`, color: `black` },
-// ];
-
-// let pixelArray;
-// for (let i = 0; i < 81; i++) {
-//   pixelArray[i] = { name: `${i}`, color: `white` };
-// }
+let clearBtn = document.getElementById("clearBtn");
 
 buildGrid()
 
 function buildGrid() {
   for (let i = 0; i < 81; i++) {
-    let pixel = `<div id="pixel-${i}" class="pixel" style="background-color: black; box-shadow: none;"></div>`;
+    let pixel = `<div id="pixel-${i}" class="pixel" style="background-color: white; box-shadow: none;"></div>`;
     grid.innerHTML += pixel;
   };
 };
 
 // Get the parent DIV, add click listener...
-grid.addEventListener("mousedown", function (pixel) {
+grid.addEventListener("mousedown", function (e) {
+  let chosenColor = document.getElementById("colorPicker").value;
   // pixel.target was the clicked element
-  if (pixel.target && pixel.target.matches("div.pixel")) {
-    const pixelColorCurr = pixel.target.style.backgroundColor;
+  console.log(chosenColor);
+
+  let pixel = e.target;
+  if (pixel && pixel.matches("div.pixel")) {
+    const pixelColorCurr = pixel.style.backgroundColor;
     console.log(pixelColorCurr);
-    console.log("Pixel element: " + pixel.target.id);
-    if (pixel.target.style.backgroundColor == "black") {
-      pixel.target.style.backgroundColor = 'white';
-    }
-    else if (pixel.target.style.backgroundColor == "white") {
-      pixel.target.style.backgroundColor = 'black';
-    }
+    console.log("Pixel element: " + pixel.id);
+    pixel.style.backgroundColor = chosenColor;
   }
 });
 
-grid.addEventListener("mouseover", function (pixel) {
+clearBtn.addEventListener("click", function () {
+  // array1.forEach(element => console.log(element));
+  for (let i = 0; i < 81; i++) {
+    let pixel = document.getElementById(`pixel-${i}`);
+    pixel.style.backgroundColor = `white`;
+  }
+});
+
+grid.addEventListener("mouseover", function (e) {
+  let pixel = e.target;
   // pixel.target was the clicked element
-  if (pixel.target && pixel.target.matches("div.pixel")) {
+  if (pixel && pixel.matches("div.pixel")) {
     const boxShadow = `0 0 6px 2px rgba(255, 255, 255, .2) inset`;
-    pixel.target.style.boxShadow = boxShadow;
+    pixel.style.boxShadow = boxShadow;
   }
 });
 
-grid.addEventListener("mouseout", function (pixel) {
+grid.addEventListener("mouseout", function (e) {
+  let pixel = e.target;
   // pixel.target was the clicked element
-  if (pixel.target && pixel.target.matches("div.pixel")) {
-    pixel.target.style.boxShadow = "none";
+  if (pixel && pixel.matches("div.pixel")) {
+    pixel.style.boxShadow = "none";
   }
 });
-
-// background-color: aqua;
